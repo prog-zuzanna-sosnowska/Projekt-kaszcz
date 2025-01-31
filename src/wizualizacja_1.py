20e0458308675c434f361d62a808dcb0fdc0b5a1
Zuzanna_Sosnowska <126651779+prog-zuzanna-sosnowska@users.noreply.github.com>
2025-01-31 20:02:40 +0100

    Dodanie folderu z wykresami; dodanie wykresu z wizualizacją do folderu img

--- src/wizualizacja_1.py
+++ src/wizualizacja_1.py
@@ -14,9 +14,16 @@ def main():
     # max_temperature = df['TMAX']
     # min_temperature = df['TMIN']
 
+    plt.figure(figsize=(9, 4))
+
     plt.plot(average_temperature)
     plt.title('Średnia temperatura w Warszawie na przełomie lat 1993-2022')
+    plt.xlabel('Czas (w dniach)')
+    plt.ylabel(r'Temperatura ($\degree$C)')
+    plt.tight_layout()
+    plt.savefig("..\\img\\wizualizacja_danych.png")
     plt.show()
+    plt.close()
 
 
 if __name__ == '__main__':
